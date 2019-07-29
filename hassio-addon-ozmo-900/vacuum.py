f182154d55a354e9e9a174b02c0267b4ea493c12
Marco Lijoi <marco.lijoi@scmitaly.it>
2019-07-29 11:26:51 +0200

    moving from print to logging

--- hassio-addon-ozmo-900/vacuum.py
+++ hassio-addon-ozmo-900/vacuum.py
@@ -6,9 +6,10 @@ import paho.mqtt.client as paho
 import json
 import random
 import string
+import sys
 
 import logging
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S', stream=sys.stdout, level=logging.DEBUG)
 
 
 class DeebotMQTTClient:
